14ce1e052ec8fd093cd3e624b85af09747293e51
Victoria <tikhomirovavs@gmail.com>
2022-11-20 01:30:18 +0300

    Закончили третий проект

--- task3/Program.cs
+++ task3/Program.cs
@@ -7,3 +7,17 @@
 Console.WriteLine("Введите число: ");
 
 int number = Convert.ToInt32(Console.ReadLine());
+
+bool Weekend (int day)
+{
+    return day == 6 || day == 7;
+}
+
+if (number < 1 || number > 7) Console.WriteLine("Вы ввели неверное число");
+else if (number > 0 && number < 6) Console.WriteLine($"{number} -> нет");
+else
+{
+    bool result = Weekend(number);
+    Console.WriteLine($"{number} -> да");
+}
+
